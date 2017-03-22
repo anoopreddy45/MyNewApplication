ef8df767802e7f6f775595df9dfe405122f815c9
Anoop <anoopreddy45@gmail.com>
2017-03-21 22:56:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/kollu/mynewapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.kollu.mynewapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Intent intent= new Intent(MainActivity.this,Main2Activity.class);
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+    }
+}
